77b9f1716d1f0e630f10abb5ed0e6e268376c724
G B <egor.workroom@gmail.com>
2017-02-09 12:57:14 +0600

    added logger

--- src/apps/echo/server.go
+++ src/apps/echo/server.go
@@ -22,5 +22,6 @@ type EchoServer struct {
 }
 
 func (s *EchoServer) Echo(_ context.Context, dto *EchoDTO) (*EchoDTO, error) {
+	s.logger.Println(">> str=%s, raw_length=%d", dto.Str, len(dto.Raw))
 	return dto, nil
 }
